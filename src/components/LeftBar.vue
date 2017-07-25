15665f9afc3ef329306b1b78bf4731a316e78a5b
linxiaoji <406611515@qq.com>
2017-07-25 20:03:13 +0800

    update

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -1,13 +1,33 @@
 <template>
-    <div id="file-list" class="file-list">
+    <div class="file-list">
         <ul>
             <li v-for="item in fileList" @click="showFile(item.id)"><a :class="{special:(item.id == currentFileId)}"><Icon type="document"></Icon>{{item.name}}</a></li>
         </ul>
-        <div class="add-file">添加文档</div>
+        <div class="add-file" @click="modal = true">添加文档</div>
+        <Modal
+                v-model="modal"
+                title="添加文档"
+                width="400"
+                :footer-hide="true"
+        >
+            <Form ref="formInline" >
+                <Form-item prop="username">
+                    <Input type="text" placeholder="请输入文档名称"/>
+                </Form-item>
+                <Form-item>
+                    <Button type="primary" @click="handleSubmit('formInline')" long>添加</Button>
+                </Form-item>
+            </Form>
+        </Modal>
     </div>
 </template>
 <script>
     export default {
+        data() {
+            return {
+                modal:false
+            }
+        },
         computed: {
             fileList() {
                 return this.$store.state.fileList
